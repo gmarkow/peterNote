2fb86de81c3d7db42bfbb097b76d5022c812360f
gmarkow <greg.markowski@gmial.com>
2017-06-13 18:57:12 -0400

    Database connecting and updating on keyup

--- noteTaker.py
+++ noteTaker.py
@@ -1,16 +1,20 @@
 from Tkinter import *
 import database
-import os.path
 
 root = Tk()
 root.geometry("600x600")
+def key(key):
+	database.upsert(text.get("1.0", "end"))
+
 text = Text(root)
 text.insert(END, 'stuff')
+text.bind("<KeyRelease>", key)
 text.pack()
 
 def get_input():
 	inputValue = text.get("1.0", "end")
 	print(inputValue)
+
 
 buttonCommit = Button(root, height=1, width=10, text="Print-it", command=lambda: database.upsert(text.get("1.0", "end")))
 buttonCommit.pack()
