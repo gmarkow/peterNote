b932462bc1dec2f065c6b49c494741e8c01188e4
gmarkow <greg.markowski@gmial.com>
2017-06-14 20:39:50 -0400

    Drag and drop working

--- noteTaker.py
+++ noteTaker.py
@@ -1,8 +1,14 @@
 from Tkinter import *
+import os
 import database
+root_path = os.path.dirname(os.path.realpath(__file__))
+os.environ['TKDND_LIBRARY'] = root_path + '/tkdnd2.8/'
+from tkdnd_wrapper import TkDND
 
 database.create_current_note()
 root = Tk()
+dnd = TkDND(root)
+
 root.geometry("600x600")
 root.configure(background='#fefbae')
 def key(key):
@@ -20,7 +26,15 @@ def render_notes():
   text.mark_set("end_notes", INSERT)
   text.mark_gravity("end_notes", LEFT)
 
+def handle(event):
+    event.widget.insert(END, event.data)
+    content = text.get("0.0", END)
+    filename = content.split()
+    key('ba')
+    print(filename)
+
 text = Text(root)
+
 # text.mark_set('<begin_note>')
 text.configure(background='#fefbae')
 render_notes()
@@ -28,7 +42,7 @@ render_notes()
 
 text.bind("<KeyRelease>", key)
 text.pack()
-
+dnd.bindtarget(text, handle, 'text/uri-list')
 
 buttonCommit = Button(root, height=1, width=10, text="Print-it", command=lambda: database.upsert(text.get("1.0", "end")))
 buttonCommit.pack()
